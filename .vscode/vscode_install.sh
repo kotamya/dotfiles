90e20001819ae207647d12cf9aea621830df9b5e
kotamya <kota.myhr@gmail.com>
2021-02-05 00:27:16 +0900

    first commit

---
+++ .vscode/vscode_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+VSCODE_SETTING_DIR = %HOME/AppData/Roaming/Code/User
+
+rm "$VSCODE_SETTING_DIR/settings.json"
+ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+
+# install extensions
+cat extensions | while read line
+do
+    code --install-extension $line
+done
+
+code --list-extensions > extensions
