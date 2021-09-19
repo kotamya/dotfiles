1523566507659c3d6b128c91643bc25bf7cced1d
kotamya <kota.myhr@gmail.com>
2021-09-19 17:22:31 +0900

    add alias

--- git/setup.sh
+++ git/setup.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 echo "----- git setup start -----"
 
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
 GITCONFIG_DIR="$HOME"
-
 ln -sf "$SCRIPT_DIR/.gitconfig" "$GITCONFIG_DIR/.gitconfig"
 
 echo "----- git setup end -----"
+
