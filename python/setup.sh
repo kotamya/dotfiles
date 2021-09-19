24e7776d553fe0f496e0e32d6dccd035eb6f6257
kotamya <kota.myhr@gmail.com>
2021-09-19 15:23:39 +0900

    change conda to pip

--- python/setup.sh
+++ python/setup.sh
@@ -1,28 +1,9 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 echo "------ python setup start ------"
 
-# conda init
-__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/Anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    \eval "$__conda_setup"
-else
-    if [ -f "$HOME/Anaconda3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/Anaconda3/etc/profile.d/conda.sh"
-        CONDA_CHANGEPS1=false conda activate base
-    else
-        \export PATH="$PATH:$HOME/Anaconda3/bin"
-    fi
-fi
-unset __conda_setup
-
-conda update -y -n base -c defaults conda
-conda config --set auto_activate_base False
-conda create -y -n mainenv python=3.9.1
-conda activate mainenv
-conda install -y -c conda-forge --file $HOME/dotfiles/python/requirements.txt
-
-conda deactivate
+pip3 install -Uq pip && pip3 --version
+pip3 install -r requirements.txt && pip3 list
 
 echo "------ python setup end ------"
 
