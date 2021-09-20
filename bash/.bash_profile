329c60fe7306a6ff74d6e75a476858361d37f5f5
kotamya <kota.myhr@gmail.com>
2021-09-20 23:22:54 +0900

    Add environmental variables for poetry

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -1,5 +1,7 @@
-export PATH="$PATH:$HOME\AppData\Local\Programs\Python\Python39"
+export PATH="$HOME/AppData/Local/Programs/Python/Python39:$PATH"
+export PYENV="$HOME/.pyenv/pyenv-win"
+export PYENV_HOME="$HOME/.pyenv/pyenv-win"
+export PATH="$HOME/.pyenv/pyenv-win/bin:$PATH"
+export PATH="$HOME/.pyenv/pyenv-win/shims:$PATH"
 
-test -f ~/.profile && . ~/.profile
 test -f ~/.bashrc && . ~/.bashrc
-
--- bash/setup.sh
+++ bash/setup.sh
@@ -5,12 +5,10 @@ echo "------ bash setup start ------"
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
 BASHRC_DIR="$HOME"
 BASHPROFILE_DIR="$HOME"
-PROFILE_DIR="$HOME"
 MINTTYRC_DIR="$HOME"
 
 ln -sf "$SCRIPT_DIR/.bashrc" "$BASHRC_DIR/.bashrc"
 ln -sf "$SCRIPT_DIR/.bash_profile" "$BASHPROFILE_DIR/.bash_profile"
-ln -sf "$SCRIPT_DIR/.profile" "$PROFILE_DIR/.profile"
 ln -sf "$SCRIPT_DIR/.minttyrc" "$MINTTYRC_DIR/.minttyrc"
 
 echo "------ bash setup end ------"
