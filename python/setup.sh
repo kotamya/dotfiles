23b091c26488732141779a5e9f1b731717d6bfe9
kotamya <kota.myhr@gmail.com>
2021-09-22 00:58:44 +0900

    Set poetry config virtualenvs.in-project: True

--- python/setup.sh
+++ python/setup.sh
@@ -5,5 +5,7 @@ echo "------ python setup start ------"
 pip3 install -Uq pip && pip3 --version
 pip3 install -r requirements.txt && pip3 list
 
+poetry config virtualenvs.in-project true 
+
 echo "------ python setup end ------"
 
