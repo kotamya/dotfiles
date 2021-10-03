aa2c5cb8ec81b01b75393b53c48a829a4239bd01
kotamya <kota.myhr@gmail.com>
2021-10-03 17:27:50 +0900

    Add pyenv-win

--- python/setup.sh
+++ python/setup.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
 pip3 install -Uq pip && pip3 --version
 pip3 install -r $SCRIPT_DIR/requirements.txt && pip3 list
+pip3 install pyenv-win -t $HOME/.pyenv --upgrade
 
 poetry config virtualenvs.in-project true 
 
