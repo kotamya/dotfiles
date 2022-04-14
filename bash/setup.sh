6cf3f750f8432246a798661087db4ac1af325e11
kotamya <kota.myhr@gmail.com>
2022-04-14 11:53:59 +0900

    Install tree command

--- bash/setup.sh
+++ bash/setup.sh
@@ -11,5 +11,9 @@ ln -sf "$SCRIPT_DIR/.bashrc" "$BASHRC_DIR/.bashrc"
 ln -sf "$SCRIPT_DIR/.bash_profile" "$BASHPROFILE_DIR/.bash_profile"
 ln -sf "$SCRIPT_DIR/.minttyrc" "$MINTTYRC_DIR/.minttyrc"
 
+curl -OL http://downloads.sourceforge.net/gnuwin32/tree-1.5.2.2-bin.zip
+sudo unzip -p tree-*.zip bin/tree.exe > "C:/Program Files/Git/cmd/tree.exe"
+rm -rf tree-.zip
+
 echo "------ bash setup end ------"
 
